f37a00b4ee5f772130ae9b1779edd3a15759eddd
JuKyYoon <yoongh97@naver.com>
2022-07-19 10:50:55 +0900

    test: junit test for updating user

--- backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
@@ -3,20 +3,26 @@ package com.ssafy.backend;
 import com.ssafy.backend.model.entity.User;
 import com.ssafy.backend.model.repository.UserRepository;
 import org.junit.After;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class BackendApplicationTests {
@@ -24,24 +30,44 @@ class BackendApplicationTests {
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeEach
+    @AfterEach
     public void cleanup() {
         // 삭제 왜 안됄까
         userRepository.deleteAllInBatch();
     }
 
-    @Test
+//    @Test
     public void insertUser() {
-        // 연결된 MySQL에 들어감
-        userRepository.save(
-                new User.Builder("testuser", "myname", "1234").build()
-        );
+            // 연결된 MySQL에 들어감
+            userRepository.save(
+                    new User.Builder("testuser", "myname", "1234").build()
+            );
 
-        List<User> userList = userRepository.findAll();
-        User user = userList.get(0);
-        assertThat(user.getId(), is("testuser"));
-        assertThat(user.getNickname(), is("myname"));
-        assertThat(user.getPassword(), is("1234"));
+            List<User> userList = userRepository.findAll();
+            User user = userList.get(0);
+            assertThat(user.getId(), is("testuser"));
+            assertThat(user.getNickname(), is("myname"));
+            assertThat(user.getPassword(), is("1234"));
     }
 
+
+    @Test
+    public void updateUser() {
+        String id = "testuser";
+        User user = userRepository.findById(id);
+
+        assertNotNull(user);
+
+        if(user != null) {
+            // 값 변경
+            user.updateInfo("newname");
+            userRepository.save(user);
+
+            List<User> userList = userRepository.findAll();
+            user = userList.get(0);
+            assertThat(user.getId(), is("testuser"));
+        } else {
+        }
+
+    }
 }
