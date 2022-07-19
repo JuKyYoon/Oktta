3cf900876bffa723361763b64c3fb427746c722b
유지태 <yoojt233@naver.com>
2022-07-19 23:52:46 +0900

    chore: add user, board dump sql file

---
+++ backend/src/main/resources/dump_user.sql
@@ -0,0 +1,7 @@
+use webdb;
+
+INSERT INTO user VALUES (1,"yoojt233@naver.com","명암밝기","12345",now(),null,0,null,1,'ROLE_USER');
+INSERT INTO user VALUES (2,"yoojt233@gmail.com","배가고파요","0000",now(),null,0,null,0,'ROLE_USER');
+INSERT INTO user VALUES (3,"yoojt21@naver.com","qwerty1010","qwerty1010",now(),null,0,null,0,'ROLE_USER');
+
+select * from user;
