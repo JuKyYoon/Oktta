98f63ade7390d9c54916cc7816a3132fbba4f262
yoojt233 <yoojt233@naver.com>
2022-07-20 09:52:39 +0900

    refactor: modified modify_date null to now

--- backend/src/main/resources/dump_board.sql
+++ backend/src/main/resources/dump_board.sql
@@ -1,7 +1,7 @@
 use webdb;
 
-INSERT INTO board VALUES (1, 1, "이렇게 하면 될까", "맞나", now(), null, 0, 0);
-INSERT INTO board VALUES (2, 1, "TEST 2", "테스트 중이에요", now(), null, 0, 0);
-INSERT INTO board VALUES (3, 1, "맞는듯", "맞을거야", now(), null, 1, 0);
+INSERT INTO board VALUES (1, 1, "이렇게 하면 될까", "맞나", now(), now(), 0, 0);
+INSERT INTO board VALUES (2, 1, "TEST 2", "테스트 중이에요", now(), now(), 0, 0);
+INSERT INTO board VALUES (3, 1, "맞는듯", "맞을거야", now(), now(), 1, 0);
 
 select * from board;
