538d46d21f12296ac056abf799c9c1f113f1b627
ebyoung5183 <ebyoung5183@naver.com>
2022-07-20 14:03:08 +0900

    feat: add homepage and set routes

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Nav from '../components/layouts/Nav';
+import Nav from '../components/layout/Nav';
 
 const Layout = () => {
   return (
