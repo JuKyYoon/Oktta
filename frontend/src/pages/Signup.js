5142d5bc2801da8a6b0bdfa4854959e51f969766
ebyoung5183 <ebyoung5183@naver.com>
2022-07-22 23:02:07 +0900

    feat: make signup ui change depending on validity

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,156 +1,177 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { FormControl, InputLabel, Input, FormHelperText, Container, Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { checkEmailRequest, checkNicknameRequest} from '../services/userService';
+import { checkEmailRequest, checkNicknameRequest, signupRequest } from '../services/userService';
 import { debounce } from 'lodash';
 
-
-
-const debounceFunc = debounce((value, func)=>{func(value)
+const debounceFunc = debounce((value, func) => {
+  func(value)
 }, 1000)
 
-
 const Signup = () => {
-
-    // 테마색 설정
-    const theme = createTheme({
-        palette: {
-            veryperi: {
-                main: '#6667AB',
-                contrastText: '#fff',
-                },
-        },
-    });
-
-
-    
-    // input값들 useState
-    const [email, setEmail] = useState("");
-    const [isEmailValid, setIsEmailValid] = useState(false);
-    const [isNicknameValid, setIsNicknameValid] = useState(false);
-    const [isPwValid, setIsPwValid] = useState(false);
-    
-    
-    const emailChange = (event) => {
-        setEmail(event.target.value);
-        const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-        setIsEmailValid(regEmail.test(event.target.value))
-        if (isEmailValid) {
-            setEmailChecked(debounceFunc(event.target.value, checkEmailRequest));
-        }
-        
-    }
-
-    const [pw, setPw] = useState("");
-    const pwChange = (event) => {
-        setPw(event.target.value);
-        if(event.target.value){
-            setIsPwValid(event.target.value === pwCheck)
-        }
-        
-    }
-
-    const [pwCheck, setPwCheck] = useState("");
-    const pwCheckChange = (event) => {
-        setPwCheck(event.target.value);
-        if(event.target.value){
-            setIsPwValid(pw === event.target.value)
-        }
-        
-    }
-
-
-
-    const [nickname, setNickname] = useState("");
-    const nicknameChange = (event) => {
-        setNickname(event.target.value);
-        
-        if(event.target.value){
-            const regNickname = /[^\w\sㄱ-힣]|[\_]/g;
-            setIsNicknameValid(!regNickname.test(event.target.value))
-            if(isNicknameValid){
-                setNicknameChecked(debounceFunc(event.target.value, checkNicknameRequest));
-            }
-        }
-        
-    }
-
-    // 이메일, 닉네임 중복체크 useState
-    // 바뀔때마다 db에서 체크하는 기능 구현하기!!!!!!!!
-    const [emailChecked, setEmailChecked] = useState(false);
-    const [nicknameChecked, setNicknameChecked] = useState(false);
-
-
-    // 회원가입버튼 클릭시 함수
-    const handleSubmit = () =>{
-        if(pw !== pwCheck){
-            alert("비밀번호를 확인해주세요")
-        } else if(emailChecked && nickNmChecked){
-            // 회원가입 요청 보내기
-        }
-    }
-
-   
+  // 테마색 설정
+  const theme = createTheme({
+    palette: {
+      veryperi: {
+        main: '#6667AB',
+        contrastText: '#fff',
+      },
+    },
+  });
 
   
-    
-    return (
-        <div>
-        
-        <ThemeProvider theme={theme}>
-        <Container maxWidth="sm">
-            
-            <h2>회원가입</h2>
-            <div>
-            <FormControl>
-                <InputLabel htmlFor="email" color='veryperi'>이메일</InputLabel>
-                <Input id="email" aria-describedby="email-helper-text" color='veryperi' value={email} onChange={emailChange}/>
-                <FormHelperText id="email-helper-text">We'll never share your email.</FormHelperText>
-            </FormControl>
-            <p>
-                {isEmailValid ? (emailChecked ? "중복됨!" : "사용할수 있음") : "이메일 형식에 맞춰주세요"}
-            </p>
-                
-            </div>
-            <br/>
-            <div>
-            <FormControl>
-                <InputLabel htmlFor="pw" color='veryperi'>비밀번호</InputLabel>
-                <Input id="pw" type='password' aria-describedby="pw-helper-text" color='veryperi' value={pw} onChange={pwChange}/>
-                <FormHelperText id="pw-helper-text">영문 + 숫자 조합으로 8자 이상으로 설정해주세요</FormHelperText>
-            </FormControl>
-            </div>
-            <br/>
-            <div>
-            <FormControl>
-                <InputLabel htmlFor="pwCheck" color='veryperi' >비밀번호 확인</InputLabel>
-                <Input id="pwCheck" type='password' aria-describedby="pwCheck-helper-text"  color='veryperi' value={pwCheck} onChange={pwCheckChange}/>
-                <FormHelperText id="pwCheck-helper-text">비밀번호가 같아야합니다.</FormHelperText>
-            </FormControl>
-            </div>
-            <br/>
-            <div>
-            <FormControl>
-                <InputLabel htmlFor="nickname" color='veryperi'>닉네임</InputLabel>
-                <Input id="nickname" aria-describedby="nickNm-helper-text"  color='veryperi' value={nickname} onChange={nicknameChange}/>
-                <FormHelperText id="nickname-helper-text">특수문자를 제외한 닉네임을 입력해주세요</FormHelperText>
-            </FormControl>
-                <p>
-                    {isNicknameValid ? (nicknameChecked ? "중복됨!" : "사용할수 있음") : "닉네임 형식에 맞춰주세요"}
-                </p>
-            </div>
-            <br/><br/>
-            <div>
-            <Button variant="contained" color="veryperi" onClick={handleSubmit} disabled={ !isPwValid}>회원가입하기</Button>
-            </div>
-            
-        </Container>
-        </ThemeProvider>
-            
+  // input값들 useState
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [nickname, setNickname] = useState('');
+  
+  // 유효성 확인 결과 변수
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isPasswordSame, setIsPasswordSame] = useState(false);
+  const [isNicknameValid, setIsNicknameValid] = useState(false);
+  
+  // 이메일, 닉네임 중복체크 useState
+  // 바뀔때마다 db에서 체크하는 기능 구현하기!!!!!!!!
+  const [emailChecked, setEmailChecked] = useState(false);
+  const [nicknameChecked, setNicknameChecked] = useState(false);
+  
+  const emailChange = (event) => {
+    const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    const isEmailValid = regEmail.test(event.target.value)
 
-        </div>
-        
-    )
-}
+    setEmail(event.target.value);
+    setIsEmailValid(isEmailValid)
+
+    if (isEmailValid) {
+      setEmailChecked(debounceFunc(event.target.value, checkEmailRequest));
+    };
+  };
+
+  const passwordChange = (event) => {
+    const regPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const isPasswordValid = regPassword.test(event.target.value)
+
+    setPassword(event.target.value);
+    setIsPasswordValid(isPasswordValid)
+
+    if (event.target.value && isPasswordValid) {
+      setIsPasswordSame(event.target.value && event.target.value === passwordCheck)
+    };
+  };
+
+  const passwordCheckChange = (event) => {
+    setPasswordCheck(event.target.value);
+    setIsPasswordSame(password === event.target.value)
+  };
+
+  const nicknameChange = (event) => {
+    setNickname(event.target.value);
+
+    if (event.target.value) {
+      const regNickname = /[^\w\sㄱ-힣]|[\_]/g;
+      setIsNicknameValid(!regNickname.test(event.target.value))
+
+      if (isNicknameValid) {
+        setNicknameChecked(debounceFunc(event.target.value, checkNicknameRequest));
+      };
+    };
+  };
+
+  // 회원가입버튼 클릭시 함수
+  const handleSubmit = (event) => {
+
+    if (!isEmailValid) {
+      alert('유효하지 않은 이메일입니다.')
+
+    } else if (emailChecked) {
+      alert('이미 사용중인 이메일입니다.')
+
+    } else if (!isPasswordSame) {
+      alert('비밀번호 확인이 일치하지 않습니다..')
+
+    } else if (!isNicknameValid) {
+      alert('닉네임에 특수문자를 사용할 수 없습니다..')
+
+    } else if (nicknameChecked) {
+      alert('이미 사용중인 닉네임입니다.')
+
+    } else {
+      signupRequest({
+        email,
+        password,
+        nickname
+      })
+    };
+  };
+
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          <h2>회원가입</h2>
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="email" color="veryperi">이메일</InputLabel>
+              <Input id="email" aria-describedby="email-helper-text" color="veryperi" value={email} onChange={emailChange} />
+              <FormHelperText id="email-helper-text" error={!!email && (!isEmailValid || emailChecked)}>
+                {email ? 
+                  (isEmailValid ? 
+                    (emailChecked ? '이미 사용중인 이메일입니다.' : '사용 가능한 이메일입니다.') : '유효하지 않은 이메일입니다.')
+                    : '이메일을 입력해 주세요.'}
+              </FormHelperText>
+            </FormControl>
+          </div>
+          <br />
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="password" color="veryperi">비밀번호</InputLabel>
+              <Input id="password" type='password' aria-describedby="password-helper-text" color="veryperi" value={password} onChange={passwordChange} />
+              <FormHelperText id="password-helper-text" error={!!password && !isPasswordValid}>
+                {isPasswordValid ? '안전한 비밀번호입니다.' : '영문 + 숫자 조합으로 8자 이상으로 설정해주세요.'}
+              </FormHelperText>
+            </FormControl>
+          </div>
+          <br />
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="passwordCheck" color="veryperi" >비밀번호 확인</InputLabel>
+              <Input id="passwordCheck" type='password' aria-describedby="passwordCheck-helper-text" color="veryperi" value={passwordCheck} onChange={passwordCheckChange} />
+              <FormHelperText id="passwordCheck-helper-text" error={!!passwordCheck && !isPasswordSame} >
+                {(!passwordCheck || isPasswordSame) ? ' ' : '비밀번호가 일치하지 않습니다.'}
+              </FormHelperText>
+            </FormControl>
+          </div>
+          <br />
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="nickname" color="veryperi">닉네임</InputLabel>
+              <Input id="nickname" aria-describedby="nickNm-helper-text" color="veryperi" value={nickname} onChange={nicknameChange} />
+              <FormHelperText id="nickname-helper-text" error={!!nickname && !isNicknameValid && !nicknameChecked}>
+                {nickname ?
+                  (isNicknameValid ?
+                    (nicknameChecked ? '이미 사용 중인 닉네임입니다.' : '사용가능한 닉네임입니다.')
+                  : '닉네임에 특수문자를 사용할 수 없습니다.')
+                : '특수문자를 제외한 닉네임을 입력해주세요.'}
+              </FormHelperText>
+            </FormControl>
+          </div>
+          <br /><br />
+          <div>
+            <Button
+              variant="contained"
+              color="veryperi"
+              onClick={handleSubmit}
+              disabled={!isEmailValid || emailChecked || !isPasswordValid || !isPasswordSame || !isNicknameValid || nicknameChecked}
+            >
+              회원가입하기
+            </Button>
+          </div>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
+};
 
 export default Signup;
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,32 +1,33 @@
-
 import {request} from './axios';
-
 
 /* 요청 URL*/
 const USER_URL = "/api/v1/users"
 
-
 export const signupRequest = (dataToSubmit) => {
-  return request("post", USER_URL+"/signup", dataToSubmit);
+  const data = request("post", USER_URL+"/signup", dataToSubmit);
+  return data;
 }
 
 export const loginRequest = (dataToSubmit) => {
-  return request("post", USER_URL+"/login", dataToSubmit)
+  console.log(`로그인: ${dataToSubmit}`);
+  const data = request("post", USER_URL+"/login", dataToSubmit);
+  return data;
 }
 
 export const logoutRequest = (dataToSubmit) => {
-  return request("post", USER_URL + "/logout", dataToSubmit)
+  const data = request("post", USER_URL + "/logout", dataToSubmit);
+  return data;
 }
 
 
 // 이메일이 중복이면 true, 이메일이 중복이 아니면 false
 export const checkEmailRequest =(dataToSubmit) => {
-  console.log("이메일 중복확인보냈음")
-  return request("get", USER_URL + "/이메일 중복체크", dataToSubmit)
+  const data = request("get", USER_URL + "/이메일 중복체크", dataToSubmit);
+  return data;
 }
 
 // 닉네임이 중복이면 true, 닉네임이 중복이 아니면 false
 export const checkNicknameRequest =(dataToSubmit) => {
-  console.log("닉네임 중복확인보냈음")
-  return request("get", USER_URL + "/닉네임 중복체크,", dataToSubmit)
+  const data = request("get", USER_URL + "/닉네임 중복체크,", dataToSubmit);
+  return data;
 }
