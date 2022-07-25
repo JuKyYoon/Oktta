6f02575b50518e51c0c130b33b7fc92c33b71128
migon <migon99@naver.com>
2022-07-25 11:08:54 +0900

    style: change indentation style of four spaces into two spaces

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,93 +1,102 @@
-import { TextField } from '@mui/material';
-import { Box } from '@mui/material';
-import { Button } from '@mui/material';
-import { Link } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import GoogleLogin from './GoogleLogin';
-
+import { Button } from "@mui/material";
+import { Link } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import GoogleLogin from "./GoogleLogin";
 
 // 색깔 생성 부분
 const theme = createTheme({
-    palette: {
-        veryperi: {
-            main: '#6667AB',
-            contrastText: '#fff',
-        },
+  palette: {
+    veryperi: {
+      main: "#6667AB",
+      contrastText: "#fff",
     },
+  },
 });
 
 function Login() {
-    // 이메일 입력 부분
-    const [email, setEmail] = useState('');
-    const onEmailHandler = (event) => {
-        setEmail(event.target.value);
+  // 이메일 입력 부분
+  const [email, setEmail] = useState("");
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  // 비밀번호 입력 부분
+  const [password, setPassword] = useState("");
+  const onPasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const dispatch = useDispatch();
+
+  // 로그인 구현 부분
+  const onSubmitHandler = (event) => {
+    const data = {
+      id: email,
+      password: password,
     };
 
-    // 비밀번호 입력 부분
-    const [password, setPassword] = useState('');
-    const onPasswordHandler = (event) => {
-        setPassword(event.target.value);
-    };
+    // dispatch(Login(data))
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+  };
 
-    // 임시 출력 부분
-    const print = () => {
-        console.log(email)
-        console.log(password)
-    };
-
-    const dispatch = useDispatch();
-
-    // 로그인 구현 부분
-    const onSubmitHandler = (event) => {
-        const data = {
-            id: email,
-            password: password,
-        };
-
-        dispatch(Login(data))
-            .then((res) => {
-                console.log(res);
-            })
-    };
-
-    return (
-        <>
-            <div className='loginform'>
-                <Box
-                    sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
-                    }}
-                >
-                    <TextField
-                        label="이메일"
-                        value={email}
-                        onChange={onEmailHandler}
-                    />
-                    <br />
-                    <TextField
-                        label="비밀번호"
-                        type="password"
-                        value={password}
-                        onChange={onPasswordHandler}
-                        autoComplete="current-password"
-                    />
-                    <br />
-                    <ThemeProvider theme={theme}>
-                        <Button variant="contained" color="veryperi" onClick={print}>로그인</Button>
-                    </ThemeProvider>
-                    <br />
-                    {/* 임시 소셜 로그인 */}
-                    <Link href="#" underline="none">네이버</Link>
-                    <Link href="#" underline="none">카카오</Link>
-                    <GoogleLogin />
-                    <br />
-                    <Link href="pwInquiry" underline="none">비밀번호 찾기</Link>
-                    <Link href="#" underline="none">회원가입</Link>
-                </Box>
-            </div></>
-    );
+  return (
+    <div className="loginform">
+      <form onSubmit={onSubmitHandler}>
+        <label>
+          이메일
+          <br />
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={onEmailHandler}
+          />
+        </label>
+        <br />
+        <label>
+          비밀번호
+          <br />
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={onPasswordHandler}
+            autoComplete="current-password"
+          />
+        </label>
+        <br />
+        <ThemeProvider theme={theme}>
+          <Button type="submit" variant="contained" color="veryperi">
+            로그인
+          </Button>
+        </ThemeProvider>
+      </form>
+      <br />
+      {/* 소셜 로그인 */}
+      <div>
+        <Link href="#" underline="none">
+          네이버
+        </Link>
+        <Link href="#" underline="none">
+          카카오
+        </Link>
+        <GoogleLogin />
+      </div>
+      <br />
+      <div>
+        <Link href="pwInquiry" underline="none">
+          비밀번호 찾기
+        </Link>
+        <Link href="#" underline="none">
+          회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
