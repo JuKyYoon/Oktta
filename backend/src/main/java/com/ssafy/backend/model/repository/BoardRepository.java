918327001c54230f12d52bb6908ac7de5c6c208e
yoojt233 <yoojt233@naver.com>
2022-08-03 14:23:22 +0900

    feat: get boards list by category and page number

--- backend/src/main/java/com/ssafy/backend/model/repository/BoardRepository.java
+++ backend/src/main/java/com/ssafy/backend/model/repository/BoardRepository.java
@@ -1,12 +1,12 @@
 package com.ssafy.backend.model.repository;
 
-import com.ssafy.backend.model.dto.BoardDto;
 import com.ssafy.backend.model.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -16,4 +16,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Modifying
     @Query("UPDATE Board board SET board.hit = board.hit+1 WHERE board.idx = :idx")
     int updateHit(Long idx);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM board WHERE category = :category LIMIT 10 OFFSET :page")
+    List<Board> findAllByCategory(int category, int page);
 }
