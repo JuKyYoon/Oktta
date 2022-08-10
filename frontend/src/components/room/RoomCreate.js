240a5be86f6abc073a5ec21ff380df6cd9d0d73e
ebyoung5183 <ebyoung5183@naver.com>
2022-08-10 23:32:44 +0900

    feat: get match data when create room

--- frontend/src/components/room/RoomCreate.js
+++ frontend/src/components/room/RoomCreate.js
@@ -1,16 +1,152 @@
 import React, { useState } from 'react';
-import { Button } from '@mui/material';
+import { Box, Button, Modal, TextField, PaginationItem } from '@mui/material';
 import { useNavigate } from 'react-router';
-import { createRoom } from '../../services/roomService';
+import { createRoom, getMatchBySummoner, getMatchDetail } from '../../services/roomService';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '../../util/build/ckeditor';
 import '@ckeditor/ckeditor5-build-classic/build/translations/ko';
 import '../../styles/room.scss';
+import Loading from '../layout/Loading';
+
+const positionKr = {
+  'TOP': '탑',
+  'JUNGLE': '정글',
+  'MIDDLE': '미드',
+  'BOTTOM': '원딜',
+  'UTILITY': '서폿',
+}
 
 const RoomCreate = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const navigate = useNavigate();
+
+  // 게임 정보 불러오기 관련
+  const [open, setOpen] = useState(false);
+  const [pageNum, setPageNum] = useState(0);
+  const [summonerName, setSummonerName] = useState('');
+  const [matchList, setMatchList] = useState([]);
+  const [matchListView, setMatchListView] = useState([]);
+  const [searchState, setSearchState] = useState('before');
+  const [matchSelected, setMatchSelected] = useState({});
+  const [matchForSubmit, setMatchForSubmit] = useState({});
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => {
+    // 검색 내역 초기화
+    setSummonerName('');
+    setMatchList([]);
+    setMatchListView([]);
+    setSearchState('before');
+    setMatchSelected('');
+    setOpen(false);
+  };
+
+  const handleSelect = () => {
+    const match = matchList.filter((match) => matchSelected === match.metadata.matchId)[0];
+    setMatchForSubmit(match[0])
+
+    // 검색 내역 초기화
+    setSummonerName('');
+    setMatchList([]);
+    setMatchListView([]);
+    setSearchState('before');
+    setMatchSelected('');
+    setOpen(false)
+  };
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    getMatches()
+    .then((result) => setSearchState(result));
+  };
+
+  const getMatches = async () => {
+    if (!summonerName) {
+      return;
+    };
+    setSearchState('pending');
+    const data = await getMatchBySummoner(summonerName, pageNum)
+      .then((res) => {
+        setSearchState('finish')
+        if (res.data.message === 'success') {
+          return res.data.matchList;
+        } else {
+          return []
+        };
+      })
+      .catch((err) => {
+        return (err.response.status === 404) ? setSearchState('fail') : alert('잘못된 접근입니다.')
+      });
+
+    console.log(data)
+
+    const matchList = []
+    for (let matchId of data) {
+      const res = await getMatchDetail(matchId)
+        .catch((err) => console.log(err));
+      matchList.push(res);
+    };
+    if (matchList.length) {
+      setMatchList(matchList);
+      const matchListView = matchList.map((match) => {
+        console.log(match)
+        const matchId = match.metadata.matchId;
+
+        const target = match.info.participants
+          .filter((participant) => participant.summonerName.toLowerCase() === summonerName.toLowerCase())[0];
+        const matchResult = target.win ? '승' : '패';
+        const championTarget = target.championName;
+        const kda = `${target.kills} / ${target.deaths} / ${target.assists}`;
+        const position = target.teamPosition ? positionKr[target.teamPosition] : '칼바람';
+
+        const championTeam1 = match.info.participants
+          .filter((participant) => participant.teamId === 100)
+          .map((participant) => participant.championName)
+
+        const championTeam2 = match.info.participants
+          .filter((participant) => participant.teamId === 200)
+          .map((participant) => participant.championName)
+
+        const fromNow = Math.round((Date.now() - match.info.gameEndTimestamp) / 1000 / 60 / 60)
+        const endTime = fromNow < 24 ? `${fromNow} 시간 전` : `${Math.round(fromNow / 24)} 일 전`
+
+        return {
+          matchId,
+          matchResult,
+          championTarget,
+          kda,
+          championTeam1,
+          championTeam2,
+          position,
+          endTime,
+        }
+      })
+      setMatchListView(() => matchListView);
+      return 'done';
+    } else {
+      return 'fail';
+    };
+  };
+
+  const onSummonerNameChanged = (e) => {
+    setSummonerName(e.target.value)
+  };
+
+  const onHandlePrev = () => {
+    setPageNum(pageNum-1);
+    getMatches()
+    .then((result) => setSearchState(result));
+  };
+  
+  const onHandleNext = () => {
+    setPageNum(pageNum+1);
+    getMatches()
+    .then((result) => setSearchState(result));
+  };
+  // ---------------------
+
   const onTitleChanged = (e) => {
     setTitle(e.target.value);
   };
@@ -20,6 +156,7 @@ const RoomCreate = () => {
     const body = {
       title,
       content,
+      matchDto: matchList[0],
     };
 
     createRoom(body)
@@ -51,6 +188,101 @@ const RoomCreate = () => {
         value={title}
         onChange={onTitleChanged}
       />
+      {/* ################## 게임 불러오기 부분 ################## */}
+      <Button onClick={handleOpen}>게임 불러오기</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box className="modal-box">
+          <form onSubmit={onSearchSubmit} className="modal-search-form">
+            <TextField
+              id="summonerName"
+              label="소환사명"
+              variant="standard"
+              color="veryperi"
+              type="text"
+              value={summonerName}
+              onChange={onSummonerNameChanged}
+            />
+            <Button
+              variant="contained"
+              color="veryperi"
+              type="submit"
+              disabled={!summonerName}
+            >
+              검색
+            </Button>
+          </form>
+          <div className="modal-result-div">
+            {
+              (searchState == 'before')
+                ? <p>소환사명을 검색해 최근 전적을 불러올 수 있습니다.</p>
+                : (searchState == 'pending')
+                  ? <Loading />
+                  : (searchState == 'fail')
+                    ? <p>소환사를 찾을 수 없습니다. 다시 검색해 주세요.</p>
+                    : <div className="modal-result-list">
+                      {matchListView.map((match, idx) =>
+                        <div
+                          key={idx}
+                          className={
+                            `${match.matchId === matchSelected ? "modal-result-item-selected" : null}
+                            ${match.matchResult === '승' ? "modal-result-item-win" : "modal-result-item-loss"}
+                            modal-result-item`
+                          }
+                          onClick={() => setMatchSelected(match.matchId)}
+                        >
+                          <span>{match.matchResult}</span>
+                          <img src={`/assets/champion/${match.championTarget}.png`} className="result-champion-target" />
+                          <span className="result-kda-span">{match.kda}</span>
+                          <span className="result-champion-all">
+                            <div className="result-champion-team">
+                              {
+                                match.championTeam1.map((champion, idx) =>
+                                  <img key={idx} src={`/assets/champion/${champion}.png`} className="result-champion-one" />
+                                )
+                              }
+                            </div>
+                            <div className="result-champion-team">
+                              {
+                                match.championTeam2.map((champion, idx) =>
+                                  <img key={idx} src={`/assets/champion/${champion}.png`} className="result-champion-one" />
+                                )
+                              }
+                            </div>
+                          </span>
+                          <span className="result-position-span">{match.position}</span>
+                          <span className="result-time-span">{match.endTime}</span>
+                        </div>
+                      )}
+                      <div className="result-pagenation">
+                        <PaginationItem
+                          type="previous"
+                          shape="rounded"
+                          variant="outlined"
+                          onClick={onHandlePrev}
+                          disabled={pageNum === 0}
+                        />
+                        <PaginationItem
+                          type="next"
+                          shape="rounded"
+                          variant="outlined"
+                          onClick={onHandleNext}
+                          disabled={matchList.length < 10}
+                        />
+                      </div>
+                    </div>
+            }
+          </div>
+          <div className="modal-button-div">
+            <Button onClick={handleSelect}>선택</Button>
+            <Button onClick={handleClose}>닫기</Button>
+          </div>
+        </Box>
+      </Modal>
+      {/* ###################################################### */}
+
       <label htmlFor='title' className='create-room-label'>
         내용
       </label>
--- frontend/src/services/roomService.js
+++ frontend/src/services/roomService.js
@@ -1,6 +1,9 @@
-import { axiosAuth } from './axios.js';
+import axios from 'axios';
+import { axiosAuth, riotRequest } from './axios.js';
 
 const ROOM_URL = 'api/v1/room';
+const LOL_URL = 'api/v1/lol';
+const RIOT_MATCH = 'match/v5/matches'
 
 // room 생성
 export const createRoom = async (dataToSubmit) => {
@@ -30,4 +33,16 @@ export const updateRoom = async (idx, dataToSubmit) => {
 export const getRoomList = async (pageNum) => {
   const payload = await axiosAuth.get(`${ROOM_URL}?page=${pageNum}`);
   return payload;
+};
+
+// 소환사명으로 최근 게임 가져오기
+export const getMatchBySummoner = async (dataToSubmit, pageNum) => {
+  const payload = await axiosAuth.get(`${LOL_URL}/match/${dataToSubmit}?page=${pageNum}`);
+  return payload;
+};
+
+// 라이엇 API에서 매치 상세정보 가져오기
+export const getMatchDetail = async (dataToSubmit) => {
+  const payload = await riotRequest.get(`${RIOT_MATCH}/${dataToSubmit}?api_key=${process.env.REACT_APP_RIOT_API_KEY}`);
+  return payload.data;
 };
