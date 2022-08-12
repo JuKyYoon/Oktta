0a3283c0e63df9387197345d9d0fa38132a97adb
JuKyYoon <yoongh97@naver.com>
2022-08-12 19:17:56 +0900

    feat: show tier icon and modify css

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
   };
 
   return (
-    <div>
+    <div className="header-main-div">
       <div className="header">
         <Link to="/">
           <img src="/../assets/logo.png" className="logo" />
