2bd65cd9d2054a9ea37e75cc13fc970d6ac5008b
JuKyYoon <yoongh97@naver.com>
2022-08-18 03:27:31 +0900

    feat: get my tier

--- frontend/src/components/board/BoardList.js
+++ frontend/src/components/board/BoardList.js
@@ -79,20 +79,20 @@ const BoardList = () => {
             <Table>
               <TableHead sx={{ borderBottom: 'solid' }}>
                 <TableRow>
-                  <TableCell align='center'>제목</TableCell>
-                  <TableCell align='center'>작성자</TableCell>
-                  <TableCell align='center'>작성일</TableCell>
-                  <TableCell align='center'>조회수</TableCell>
+                  <TableCell align='center' width="50%">제목</TableCell>
+                  <TableCell align='center'  width="24%">작성자</TableCell>
+                  <TableCell align='center'  width="16%">작성일</TableCell>
+                  <TableCell align='center'  width="10%">조회수</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {boards.map((board) => (
-                  <TableRow key={board.idx} onClick={() => boardHit(`${board.idx}`)} hover>
-                    <TableCell align='center'>
+                  <TableRow key={board.idx} onClick={() => navigate(`../${board.idx}`)} hover>
+                    <TableCell>
                       {board.title}
                     </TableCell>
-                    <TableCell align='center'>{board.nickname}</TableCell>
-                    <TableCell align='center'>
+                    <TableCell>{board.nickname}</TableCell>
+                    <TableCell  align='center'>
                       {dateFormat(board.createDate)}
                     </TableCell>
                     <TableCell align='center'>{board.hit}</TableCell>
--- frontend/src/components/user/TierAuth.js
+++ frontend/src/components/user/TierAuth.js
@@ -4,24 +4,24 @@ import { Button } from '@mui/material';
 
 const TierAuth = () => {
   return (
-    <>
-    <h1 className='title'>롤 티어 인증하는 방법</h1><br/>
-    <h3 className='contents'>STEP 1. 롤 티어 인증 프로그램을 다운 받습니다.</h3>
-    <a href="https://oktta.s3.us-east-2.amazonaws.com/oktta+Setup+0.0.1.exe" download="oktta.exe">
-      <Button size='large'>티어 인증 프로그램 다운</Button>
-    </a><br/><br/>
-    <h3 className='contents'>STEP 2. OKTTA 홈페이지에서 가입한 아이디로 로그인 해주세요.</h3>
-    <img src='../assets/tier_auth/step1.png' className='tier-auth-steps' alt='1단계 사진'/><br/><br/>
-    <h3 className='contents'>STEP 3. 롤을 실행시킨 후 티어 인증하기 버튼을 클릭 해주세요.</h3>
-    <img src='../assets/tier_auth/step2.png' className='tier-auth-steps' alt='2단계 사진'/><br/><br/>
-    <h3 className='contents'>주의 : 롤을 실행시키지 않으면 다음과 같은 화면이 뜹니다.</h3>
-    <img src='../assets/tier_auth/step3.png' className='tier-auth-steps' alt='3단계 사진'/><br/><br/>
-    <h3 className='contents'>STEP 4. 롤 티어 인증 실패 시 다음과 같이 뜨게 됩니다.</h3>
-    <img src='../assets/tier_auth/step4.png' className='tier-auth-steps' alt='4단계 사진'/>
-    {/* 성공 이미지
-    <h3 className='contents'>STEP 5. 롤 티어 인증 성공 시 다음과 같이 뜨게 됩니다.</h3>
-    <img src='../assets/tier_auth/step5.png' className='tier-auth-steps' alt='5단계 사진'/> */}
-    </>
+    <div className="tier-auth-div">
+      <h1 className='title'>롤 티어 인증하는 방법</h1><br/>
+      <h3 className='contents'>STEP 1. 롤 티어 인증 프로그램을 다운 받습니다.</h3>
+      <a href="https://oktta.s3.us-east-2.amazonaws.com/oktta+Setup+0.0.1.exe" download="oktta.exe">
+        <Button size='large'>티어 인증 프로그램 다운</Button>
+      </a><br/><br/>
+      <h3 className='contents'>STEP 2. OKTTA 홈페이지에서 가입한 아이디로 로그인 해주세요.</h3>
+      <img src='../assets/tier_auth/step1.png' className='tier-auth-steps' alt='1단계 사진'/><br/><br/>
+      <h3 className='contents'>STEP 3. 롤을 실행시킨 후 티어 인증하기 버튼을 클릭 해주세요.</h3>
+      <img src='../assets/tier_auth/step2.png' className='tier-auth-steps' alt='2단계 사진'/><br/><br/>
+      <h3 className='contents'>주의 : 롤을 실행시키지 않으면 다음과 같은 화면이 뜹니다.</h3>
+      <img src='../assets/tier_auth/step3.png' className='tier-auth-steps' alt='3단계 사진'/><br/><br/>
+      <h3 className='contents'>STEP 4. 롤 티어 인증 실패 시 다음과 같이 뜨게 됩니다.</h3>
+      <img src='../assets/tier_auth/step4.png' className='tier-auth-steps' alt='4단계 사진'/>
+      {/* 성공 이미지
+      <h3 className='contents'>STEP 5. 롤 티어 인증 성공 시 다음과 같이 뜨게 됩니다.</h3>
+      <img src='../assets/tier_auth/step5.png' className='tier-auth-steps' alt='5단계 사진'/> */}
+    </div>
   )
 }
 
